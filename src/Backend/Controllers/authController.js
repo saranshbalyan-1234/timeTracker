a4ae7d9b241172cef672f533da1a511f69a994e4
saransh balyan <saransh@kditsolutions.com>
2022-08-01 01:59:43 +0530

    tracking setup

--- src/Backend/Controllers/authController.js
+++ src/Backend/Controllers/authController.js
@@ -1,14 +1,14 @@
 const db = require("../Utils/dataBaseConnection");
 const bcrypt = require("bcryptjs");
-const { createToken } = require("../Utils/jwt");
+
 const { userLoginValidation } = require("../Utils/hapiValidation");
 const getError = require("../Utils/sequelizeError");
 const User = db.users;
-const e = require("express");
+
 const login = async (req, res) => {
   const { error } = userLoginValidation.validate(req.body);
-  // if (error)
-  //   return res.status(400).json({ errors: [error.details[0].message] });
+  if (error)
+    return res.status(400).json({ errors: [error.details[0].message] });
 
   const { email, password } = req.body;
   User.findOne({ where: { email } })
@@ -18,18 +18,11 @@ const login = async (req, res) => {
       bcrypt.compare(password, resp.password, async (err, result) => {
         if (!result)
           return res.status(400).json({ errors: ["Incorrect Password"] });
-        const { id, email, name, verifiedAt } = resp;
+        const { id, verifiedAt } = resp;
         if (!verifiedAt)
           return res.status(400).json({ errors: ["Email Not Verified"] });
 
-        let tokenData = { id, email };
-        const accessToken = await createToken(
-          tokenData,
-          process.env.JWT_ACCESS_SECRET
-          // process.env.JWT_ACCESS_EXPIRATION
-        );
-
-        res.status(200).json({ id, name, email, accessToken });
+        res.status(200).json({ id });
       });
     })
     .catch((e) => {
--- src/Backend/index.js
+++ src/Backend/index.js
@@ -9,18 +9,23 @@ const activityTracker = new ActivityTracker("tracking.json", 2000);
 app.use(parser.json());
 app.use("/auth", authRoutes);
 
-app.post("/start", validateToken, (req, res) => {
+app.post("/start", (req, res) => {
   activityTracker.init();
   return res.status(200).json({ status: "started" });
 });
-app.post("/stop", validateToken, (req, res) => {
+app.post("/stop", (req, res) => {
   activityTracker.stop();
   return res.status(200).json({ status: "stopped" });
 });
 
-// app.get("/get-chart", async (req, res) => {
-//   const chartData = await activityTracker.getChartData();
-//   return res.status(200).json(chartData);
-// });
+app.post("/get-chart", async (req, res) => {
+  const chartData = await activityTracker.getChartData();
+  return res.status(200).json(chartData);
+});
+
+app.post("/save-tracking", async (req, res) => {
+  const chartData = await activityTracker.getChartData();
+  return res.status(200).json(chartData);
+});
 
 app.listen(3000, () => console.log(`Listening on port 3000!`));
